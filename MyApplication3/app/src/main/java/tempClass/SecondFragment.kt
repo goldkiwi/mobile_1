694324d1815eb01674741d2e78b543f7e42bc82b
CHOCOBANANA\subin <subin7861@gmail.com>
2021-01-05 19:07:36 +0900

    파일정리 끝

--- MyApplication3/app/src/main/java/tempClass/SecondFragment.kt
+++ MyApplication3/app/src/main/java/tempClass/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package tempClass
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.myapplication.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
