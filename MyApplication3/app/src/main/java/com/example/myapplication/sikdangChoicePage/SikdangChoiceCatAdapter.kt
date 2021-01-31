cacfe59471ef78ec85807c4dd887cdb664317171
CHOCOBANANA\subin <subin7861@gmail.com>
2021-01-31 16:05:24 +0900

    마지막 토글 버튼이 안꺼지는 오류 수정 if (lastBindCount>i)   =>   if (lastBindCount>=i)

--- MyApplication3/app/src/main/java/com/example/myapplication/sikdangChoicePage/SikdangChoiceCatAdapter.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/sikdangChoicePage/SikdangChoiceCatAdapter.kt
@@ -15,20 +15,16 @@ class SikdangChoiceCatAdapter(var context : Context, val catArrayList: ArrayList
     var catArrayListSize = catArrayList?.size!!
     var toggleArrayList= Array<Boolean>(catArrayListSize, {false})
     var firstCatCount = getCount(selectedCat)
-    var lastBindCount = 0
-    var isFirst = true
+    var lastBindCount = 0 //현재 바인드 되어있는 최대 숫자
+    var isFirst = true //처음 클린한 것인가
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        //Log.d("종료지점확인 SikdangChoiceCatAdapter", "onCreateViewHolder 시작")
         val view = LayoutInflater.from(context).inflate(R.layout.sikdangchoice_cat, parent, false)
-        //Log.d("종료지점확인 SikdangChoiceCatAdapter", "onCreateViewHolder 2")
         return Holder(view)
     }
 
     override fun getItemCount(): Int {
-        //Log.d("종료지점확인 SikdangChoiceCatAdapter", "getItempCount()")
-        //Log.d("종료지점확인 SikdangChoiceCatAdapter", "getItempCount()"+catArrayList?.size.toString())
         return catArrayListSize
     }
 
@@ -55,33 +51,32 @@ class SikdangChoiceCatAdapter(var context : Context, val catArrayList: ArrayList
 
 
 
-
     inner class Holder (itemView: View?) : RecyclerView.ViewHolder(itemView!!){
 
 
         fun bind(){
             var sikdangChoice_toggleButton : ToggleButton = itemView.findViewById(R.id.sikdangchice_toggleButton)
-            var catName: String? = catArrayList?.get(position)
+            var catName: String? = catArrayList?.get(position)//이번에 바인드하는 카테고리명
             var catCount = getCount(catName)
-            if (lastBindCount < catCount) lastBindCount = catCount
-            sikdangChoice_toggleButton_arrayList[catCount] = (sikdangChoice_toggleButton)
+            if (lastBindCount < catCount) lastBindCount = catCount//바인드되어있는 최대 숫자 커지면 수정함
+            sikdangChoice_toggleButton_arrayList[catCount] = (sikdangChoice_toggleButton)//토글버튼리스트에 바인드된 토글버튼 넣음
 
 
 
-            Log.d("종료지점확인 SikdangChoiceCatAdapter", "holder.bind()"+selectedCat+catName)
+            //Log.d("종료지점확인 SikdangChoiceCatAdapter", "holder.bind()"+selectedCat+catName)
 
+            //처음 바인드 될 때 클릭해서 들어온 버튼의 카테고리 킴
             if ((catName == selectedCat)&& (isFirst==true)){
                 sikdangChoice_toggleButton.setChecked(true)
                 toggleArrayList[firstCatCount] = true
                 isFirst=false
             }
+            //토글버튼 바인드
             sikdangChoice_toggleButton.text=catName
             sikdangChoice_toggleButton.textOn=catName
             sikdangChoice_toggleButton.textOff=catName
-            if (selectedCat == catName){
-                Log.d("확인 SikdangChoiceCatAdapter if ", selectedCat+catName)
-                //sikdangChoice_toggleButton.toggle()
-            }
+
+            //토글리스트에 버튼이 켜져있는상태인지 확인하고 켜져있는 상태이면 버튼 킴, 아니면 버튼 끔
             if (toggleArrayList[catCount] == true){
                 sikdangChoice_toggleButton.setChecked(true)
             }
@@ -89,56 +84,36 @@ class SikdangChoiceCatAdapter(var context : Context, val catArrayList: ArrayList
                 sikdangChoice_toggleButton.setChecked(false)
             }
             //토글버튼을 ArrayList에 추가해 이 클래스 밖(SikdangChoice 클래스)에세 토글버튼을 사용할 수 있게 한다
-            //onClickListener 사용해서 하나의 버튼이 켜지면 나머지 버튼은 꺼지도록 - 아예 SikdangChoice 클래스에서 구현핟고록 하는게 나을듯
-            //sikdangChoice_toggleButton_arrayList.add(sikdangChoice_toggleButton)
-
 
 
             //토글 버튼 클릭했을 때
 
             sikdangChoice_toggleButton.setOnClickListener {
-                Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", "클릭")
+                //Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", "클릭")
                 if (sikdangChoice_toggleButton.isChecked){//꺼진 버튼 누를 시 켜지고 다른 버튼 꺼지도록 한다
-                    Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "isChecked"+selectedCat+catName)
+                    //Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "isChecked"+selectedCat+catName)
                     var i = 0
-                    //catArrayListSize 를 넣으면 리사이클러뷰이기 때문에 아직 바인드하지 않은 부분까지 가서 에러발생
-                    //현재 바인드 되있는 부분까지만 가야함
                     var catCount = getCount(catName)
-                    while (i< sikdangChoice_toggleButton_arrayList.size){
-                        Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "와일문 돔"+i.toString() +"/"+sikdangChoice_toggleButton_arrayList.size.toString())
+                    while (i< sikdangChoice_toggleButton_arrayList.size){//와일문 전체 돈다
+                        //Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "와일문 돔"+i.toString() +"/"+sikdangChoice_toggleButton_arrayList.size.toString())
                         if(i != catCount){
                             toggleArrayList[i] = false//불리스트 수정
-                            Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", i.toString()+" "+catArrayList?.get(i)+" "+"토글 오프")
-                            if (lastBindCount>i)sikdangChoice_toggleButton_arrayList[i].isChecked=false//토글 끔
-                            //Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "isChecked 수정 후")
+                            //Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", i.toString()+" "+catArrayList?.get(i)+" "+"토글 오프")
+                            if (lastBindCount>=i)sikdangChoice_toggleButton_arrayList[i].isChecked=false//토글 끔 if 는 바인드된 버튼 위치가 앞쪽이면 뒤쪽의 토글버튼은 건드리지 않는다
                         }
                         else if (i == catCount){
-                            Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", catArrayList?.get(i).toString() + "토글 온")
+                            //Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", catArrayList?.get(i).toString() + "토글 온")
                             toggleArrayList[i] = true//불리스트 수정
-                            if (lastBindCount>i)sikdangChoice_toggleButton_arrayList[i].isChecked=true
+                            if (lastBindCount>=i)sikdangChoice_toggleButton_arrayList[i].isChecked=true//토글 킴 if 는 바인드된 버튼 위치가 앞쪽이면 뒤쪽의 토글버튼은 건드리지 않는다
                         }
                         i++
                     }
-                    Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", "와일문 끝")
-
-
+                    //Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", "와일문 끝")
 
                 }else{//이미 켜진 버튼 눌러도 변화 없도록 함
                     sikdangChoice_toggleButton.setChecked(true)
                 }
-                var i = 0
-                var tempString=""
-                Log.d("확인 sikdangChoice_toggleButton.setOnClickListener", "불배열 출력전")
-                while (i< catArrayListSize){
-                    if (toggleArrayList[i] == true){
-                        tempString+="1"
-                    }
-                    else{
-                        tempString+="0"
-                    }
-                    i++
-                }
-                Log.d("확인 SikdangChoiceCatAdapter   sikdangChoice_toggleButton.setOnCheckedChangeListener ", "불배열확인"+tempString)
+
             }
 
 
