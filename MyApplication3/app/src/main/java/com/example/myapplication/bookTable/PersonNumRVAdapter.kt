31856573d63d904f47ce96620787b8cb440f900d
CHOCOBANANA\subin <subin7861@gmail.com>
2021-02-11 05:49:02 +0900

    다이얼로그 인원수버튼 선택하면 이미 켜져있는 다른 버튼은 꺼지도록 함

--- MyApplication3/app/src/main/java/com/example/myapplication/bookTable/PersonNumRVAdapter.kt
+++ MyApplication3/app/src/main/java/com/example/myapplication/bookTable/PersonNumRVAdapter.kt
@@ -2,6 +2,7 @@ package com.example.myapplication.bookTable
 
 import android.content.Context
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ import com.example.myapplication.R
 //BookPersonDialog에서 사용
 //다이얼로그의 인원수 나타내는 버튼을 리사이클러뷰에 바인드
 class PersonNumRVAdapter(var context: Context, val maxP:Int, val bookPersonDialog: BookPersonDialog, val floor:Int, var curP:Int):RecyclerView.Adapter<PersonNumRVAdapter.Holder>() {
+    var numButtonAR = ArrayList<Button>()
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -41,8 +43,17 @@ class PersonNumRVAdapter(var context: Context, val maxP:Int, val bookPersonDialo
             button.setOnClickListener {
                 //버튼 클릭하면 이전 액티비티로 데이터 전달
                 button.setBackgroundColor(Color.parseColor("#55CC55"))
+                var i = 0
+                while(i<numButtonAR.size){
+                    if (i!=count){
+                        Log.d("확인 버튼 설정", i.toString()+ pos.toString()+count.toString())
+                        numButtonAR[i].setBackgroundColor(Color.parseColor("#FFFFFF"))
+                    }
+                    i++
+                }
                 bookPersonDialog.buttonClicked(pNum)
             }
+            numButtonAR.add(button)
 
         }
 
